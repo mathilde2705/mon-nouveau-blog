0090cc89a40fc419b518855d601f6a9a2544b082
Mathilde <mathly2799@hotmail.fr>
2024-03-26 14:15:02 +0100

    Ajout de la vue et du template de détail blog post ainsi que du CSS pour le site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name = 'post_list'),
+    path('nouvelle-page/', views.nouvelle_page, name='nouvelle_page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def nouvelle_page(request):
+    # Votre logique de vue ici
+    return render(request, 'blog/nouvelle_page.html')
